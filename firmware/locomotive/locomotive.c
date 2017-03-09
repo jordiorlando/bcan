55bf7b708dd25268a781c0ff2d2eead802b4dce4
Jordi Pakey-Rodriguez <jordi.orlando@gmail.com>
2017-03-09 16:11:58 -0600

    firmware: locomotive: Add systick blink for STM32-H405

--- firmware/locomotive/locomotive.c
+++ firmware/locomotive/locomotive.c
@@ -1,75 +1,63 @@
 #include <stdlib.h>
 
-#include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/usb/usbd.h>
-#include <libopencm3/usb/msc.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
 
+#include "rcc_config/rcc_config.h"
 #include "evq/evq.h"
 
-const struct rcc_clock_scale rcc_hse_16mhz_2v7_3v6[RCC_CLOCK_3V3_END] = {
-	{ /* 48MHz */
-		.pllm = 8,
-		.plln = 96,
-		.pllp = 4,
-		.pllq = 4,
-		.pllr = 0,
-		.hpre = RCC_CFGR_HPRE_DIV_NONE,
-		.ppre1 = RCC_CFGR_PPRE_DIV_2,
-		.ppre2 = RCC_CFGR_PPRE_DIV_NONE,
-		.power_save = 1,
-		.flash_config = FLASH_ACR_DCE | FLASH_ACR_ICE | FLASH_ACR_LATENCY_2WS,
-		.ahb_frequency  = 48000000,
-		.apb1_frequency = 24000000,
-		.apb2_frequency = 48000000,
-	},
-	{ /* 84MHz */
-		.pllm = 8,
-		.plln = 168,
-		.pllp = 4,
-		.pllq = 7,
-		.pllr = 0,
-		.hpre = RCC_CFGR_HPRE_DIV_NONE,
-		.ppre1 = RCC_CFGR_PPRE_DIV_2,
-		.ppre2 = RCC_CFGR_PPRE_DIV_NONE,
-		.power_save = 1,
-		.flash_config = FLASH_ACR_DCE | FLASH_ACR_ICE | FLASH_ACR_LATENCY_3WS,
-		.ahb_frequency  = 84000000,
-		.apb1_frequency = 42000000,
-		.apb2_frequency = 84000000,
-	},
-	{ /* 120MHz */
-		.pllm = 8,
-		.plln = 120,
-		.pllp = 2,
-		.pllq = 5,
-		.pllr = 0,
-		.hpre = RCC_CFGR_HPRE_DIV_NONE,
-		.ppre1 = RCC_CFGR_PPRE_DIV_4,
-		.ppre2 = RCC_CFGR_PPRE_DIV_2,
-		.power_save = 1,
-		.flash_config = FLASH_ACR_DCE | FLASH_ACR_ICE | FLASH_ACR_LATENCY_4WS,
-		.ahb_frequency  = 120000000,
-		.apb1_frequency = 30000000,
-		.apb2_frequency = 60000000,
-	},
-	{ /* 168MHz */
-		.pllm = 8,
-		.plln = 168,
-		.pllp = 2,
-		.pllq = 7,
-		.pllr = 0,
-		.hpre = RCC_CFGR_HPRE_DIV_NONE,
-		.ppre1 = RCC_CFGR_PPRE_DIV_4,
-		.ppre2 = RCC_CFGR_PPRE_DIV_2,
-		.flash_config = FLASH_ACR_DCE | FLASH_ACR_ICE | FLASH_ACR_LATENCY_6WS,
-		.ahb_frequency  = 168000000,
-		.apb1_frequency = 42000000,
-		.apb2_frequency = 84000000,
-	},
-};
+#define CLOCK_CONFIG rcc_hse_8mhz_2v7_to_3v6[RCC_CLOCK_3V3_168MHZ]
+
+
+/* Number of milliseconds since reset (overflows every 49 days). */
+volatile uint32_t system_millis;
+
+void sys_tick_handler(void) {
+	system_millis++;
+}
+
+static void clock_setup(void) {
+	/* Enable system clock at 168MHz. */
+	rcc_clock_setup_hse_3v3(&CLOCK_CONFIG);
+
+	/* Enable GPIOC clock. */
+	rcc_periph_clock_enable(RCC_GPIOC);
+}
+
+static void gpio_setup(void) {
+	/* Set GPIO12 (in GPIO port C) to 'output push-pull'. */
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+}
+
+static void systick_setup(void) {
+	/* Clock rate / 1000 to get 1ms interrupt rate. */
+	systick_set_reload(CLOCK_CONFIG.ahb_frequency / 1000);
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+	systick_counter_enable();
+	systick_interrupt_enable();
+}
+
+static void sleep_ms(uint32_t delay) {
+	uint32_t wake = system_millis + delay;
+	while (system_millis <= wake);
+}
 
 int main(void) {
-	rcc_clock_setup_hse_3v3(&rcc_hse_16mhz_2v7_3v6[RCC_CLOCK_3V3_168MHZ]);
+	int i;
+
+	clock_setup();
+	gpio_setup();
+	systick_setup();
+
+	gpio_set(GPIOC, GPIO12);
+
+	/* Blink the LED (PC12) on the board. */
+	while (1) {
+		gpio_toggle(GPIOC, GPIO12);
+		sleep_ms(500);
+	}
+
+	return 0;
 }
