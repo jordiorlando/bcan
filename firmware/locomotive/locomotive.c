3d4ac13be404ae89f1f272409884c0ae47403d68
Jordi Pakey-Rodriguez <jordi.orlando@gmail.com>
2017-03-09 22:22:45 -0600

    firmware: locomotive: Capture button press with EXTI

--- firmware/locomotive/locomotive.c
+++ firmware/locomotive/locomotive.c
@@ -2,6 +2,7 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/exti.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 
@@ -14,21 +15,13 @@
 /* Number of milliseconds since reset (overflows every 49 days). */
 volatile uint32_t system_millis;
 
-void sys_tick_handler(void) {
-	system_millis++;
-}
+/* GPIO Port A state */
+volatile uint16_t exti_gpioa_state;
+
 
 static void clock_setup(void) {
 	/* Enable system clock at 168MHz. */
 	rcc_clock_setup_hse_3v3(&CLOCK_CONFIG);
-
-	/* Enable GPIOC clock. */
-	rcc_periph_clock_enable(RCC_GPIOC);
-}
-
-static void gpio_setup(void) {
-	/* Set GPIO12 (in GPIO port C) to 'output push-pull'. */
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
 }
 
 static void systick_setup(void) {
@@ -37,6 +30,45 @@ static void systick_setup(void) {
 	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
 	systick_counter_enable();
 	systick_interrupt_enable();
+}
+
+void sys_tick_handler(void) {
+	system_millis++;
+}
+
+static void gpio_setup(void) {
+	/* Enable GPIOA clock. */
+	rcc_periph_clock_enable(RCC_GPIOA);
+	/* Enable GPIOC clock. */
+	rcc_periph_clock_enable(RCC_GPIOC);
+
+	/* Set PA0 to 'input floating'. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
+	/* Set PC12 to 'output push-pull'. */
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+}
+
+static void exti_setup(void) {
+	/* Enable EXTI0 interrupt. */
+	nvic_enable_irq(NVIC_EXTI0_IRQ);
+
+	/* Configure the EXTI subsystem. */
+	exti_select_source(EXTI0, GPIOA);
+	exti_set_trigger(EXTI0, EXTI_TRIGGER_BOTH);
+	exti_enable_request(EXTI0);
+}
+
+void exti0_isr(void) {
+	exti_gpioa_state = gpio_port_read(GPIOA);
+
+	/* Turn on LED (PC12, active low) when button (PA0, active low) is pressed. */
+	if (exti_gpioa_state & GPIO0) {
+		gpio_set(GPIOC, GPIO12);
+	} else {
+		gpio_clear(GPIOC, GPIO12);
+	}
+
+	exti_reset_request(EXTI0);
 }
 
 static void sleep_ms(uint32_t delay) {
@@ -48,15 +80,19 @@ int main(void) {
 	int i;
 
 	clock_setup();
-	gpio_setup();
 	systick_setup();
+	gpio_setup();
+	exti_setup();
 
+	/* Turn off the LED. */
 	gpio_set(GPIOC, GPIO12);
 
-	/* Blink the LED (PC12) on the board. */
+	/* Blink the on-board LED. */
 	while (1) {
+	#ifdef BLINK
 		gpio_toggle(GPIOC, GPIO12);
 		sleep_ms(500);
+	#endif
 	}
 
 	return 0;
