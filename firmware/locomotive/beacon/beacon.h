9c73d19bdbfee3bc9b26c20a5ec173ffd945834f
Jordi Pakey-Rodriguez <jordi.orlando@gmail.com>
2017-03-14 20:32:55 -0500

    firmware: locomotive: Add missing definition
    Temporary fix until change is pulled upstream

--- firmware/locomotive/beacon/beacon.h
+++ firmware/locomotive/beacon/beacon.h
@@ -10,6 +10,9 @@
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/timer.h>
 
+/* CC4NP: Capture/compare 4 complementary output polarity */
+#define TIM_CCER_CC4NP (1 << 15)
+
 void beacon_setup(void);
 bool beacon_available(void);
 uint16_t beacon_parse(void);
