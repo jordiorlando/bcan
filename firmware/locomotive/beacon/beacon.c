5b0a99bf7308287f90c3e95d0d3f79d918b7a08c
Jordi Pakey-Rodriguez <jordi.orlando@gmail.com>
2017-04-09 23:58:21 -0500

    firmware: locomotive: Fix beacon code
    Forgot to enable RCC peripheral clock for GPIOB...

--- firmware/locomotive/beacon/beacon.c
+++ firmware/locomotive/beacon/beacon.c
@@ -1,76 +1,27 @@
 #include "beacon.h"
 
+/* Protocol specifications */
 #define BIT_PERIOD    960
 #define NUM_BITS      16
 #define BUFFER_LENGTH ((2 * NUM_BITS) + 2)
-#define CH34_PORT     GPIOB
-#define CH3_PIN       GPIO0
-#define CH4_PIN       GPIO1
-
+/* 1MHz */
+#define TIMER_FREQUENCY 1000000
+/* TIM3 CH3 */
+#define PIN_R GPIO0
+#define IC_R  TIM_IC3
+/* TIM3 CH4 */
+#define PIN_L GPIO1
+#define IC_L  TIM_IC4
 
 volatile bool is_reading = false;
+volatile bool is_parsed = true;
 volatile uint8_t received = 0;
 volatile uint16_t buffer[BUFFER_LENGTH];
 volatile uint16_t sensor = 0;
 volatile uint32_t total = 0;
 
+uint16_t beacon_id = 0x0000;
 
-static void beacon_gpio_setup(void) {
-	/* Set PB0 and PB1 to 'alternate function floating'. */
-	gpio_mode_setup(CH34_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN, CH3_PIN | CH4_PIN);
-
-	/* Set PB0 and PB1 alternate function to TIM3 CH3 and CH4. */
-	gpio_set_af(CH34_PORT, GPIO_AF2, CH3_PIN | CH4_PIN);
-}
-
-static void beacon_timer_setup(void) {
-	/* Enable TIM3 clock. */
-	rcc_periph_clock_enable(RCC_TIM3);
-
-	/* Enable TIM3 interrupt. */
-	nvic_enable_irq(NVIC_TIM3_IRQ);
-
-	/* Reset TIM3 peripheral to defaults. */
-	timer_reset(TIM3);
-
-	/* Set timer mode: 2x clock divider, edge aligned, direction up. */
-	timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT_MUL_2, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-
-	/* Only generate update event on overflow. */
-	timer_update_on_overflow(TIM3);
-
-	/* Set the prescaler to have the timer run at 1MHz. */
-	timer_set_prescaler(TIM3, (rcc_apb1_frequency / 1000000) - 1);
-
-	/* Count up to 0x7FFF (~32.7ms) before timeout event. */
-	timer_set_period(TIM3, 0x7FFF);
-
-	/* Set channel direction and used input. */
-	timer_ic_set_input(TIM3, TIM_IC3, TIM_IC_IN_TI3);
-	timer_ic_set_input(TIM3, TIM_IC4, TIM_IC_IN_TI4);
-
-	/* Set input capture filter and sampling frequency. */
-	timer_ic_set_filter(TIM3, TIM_IC3, TIM_IC_OFF);
-	timer_ic_set_filter(TIM3, TIM_IC4, TIM_IC_OFF);
-
-	/* Set input capture polarity to noninverted/both edges. */
-	TIM3_CCER |= (TIM_CCER_CC3NP | TIM_CCER_CC3P);
-	TIM3_CCER |= (TIM_CCER_CC4NP | TIM_CCER_CC4P);
-
-	/* Enable input capture. */
-	timer_ic_enable(TIM3, TIM_IC3);
-	timer_ic_enable(TIM3, TIM_IC4);
-
-	/* Enable input capture interrupts. */
-	timer_enable_irq(TIM3, TIM_DIER_CC3IE);
-	timer_enable_irq(TIM3, TIM_DIER_CC4IE);
-
-	/* Enable update interrupt. */
-	timer_enable_irq(TIM3, TIM_DIER_UIE);
-
-	/* Enable counter. */
-	timer_enable_counter(TIM3);
-}
 
 void tim3_isr(void) {
 	uint16_t pin = 0;
@@ -79,11 +30,11 @@ void tim3_isr(void) {
 	if (timer_get_flag(TIM3, TIM_SR_CC3IF)) {
 		timer_clear_flag(TIM3, TIM_SR_CC3IF);
 		pulse_length = TIM3_CCR3;
-		pin = CH3_PIN;
+		pin = PIN_R;
 	} else if (timer_get_flag(TIM3, TIM_SR_CC4IF)) {
 		timer_clear_flag(TIM3, TIM_SR_CC4IF);
 		pulse_length = TIM3_CCR4;
-		pin = CH4_PIN;
+		pin = PIN_L;
 	} else if (timer_get_flag(TIM3, TIM_SR_UIF)) {
 		/* Timeout has ocurred, prepare for a new beacon. */
 		timer_clear_flag(TIM3, TIM_SR_UIF);
@@ -124,7 +75,71 @@ void tim3_isr(void) {
 			total += pulse_length;
 			received++;
 		}
+
+		if ((received == BUFFER_LENGTH) || (total >= ((NUM_BITS + 8) * BIT_PERIOD))) {
+			is_parsed = false;
+		}
 	}
+}
+
+static void beacon_gpio_setup(void) {
+	/* Enable GPIOB clock. */
+	rcc_periph_clock_enable(RCC_GPIOB);
+}
+
+static void beacon_timer_setup(void) {
+	/* Set PB0 and PB1 to 'alternate function floating'. */
+	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, PIN_R | PIN_L);
+
+	/* Set PB0 and PB1 to alternate function 2. */
+	gpio_set_af(GPIOB, GPIO_AF2, PIN_R | PIN_L);
+
+	/* Enable TIM3 clock. */
+	rcc_periph_clock_enable(RCC_TIM3);
+
+	/* Enable TIM3 interrupt. */
+	nvic_enable_irq(NVIC_TIM3_IRQ);
+
+	/* Reset TIM3 peripheral to defaults. */
+	timer_reset(TIM3);
+
+	/* Set timer mode: 2x clock divider, edge aligned, direction up. */
+	timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT_MUL_2, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+
+	/* Only generate update event on overflow. */
+	timer_update_on_overflow(TIM3);
+
+	/* Set the prescaler to have the timer run at the correct frequency. */
+	timer_set_prescaler(TIM3, (rcc_apb1_frequency / TIMER_FREQUENCY) - 1);
+
+	/* Count up to 0x7FFF (~32.7ms) before timeout event. */
+	timer_set_period(TIM3, 0x7FFF);
+
+	/* Set channel direction and used input. */
+	timer_ic_set_input(TIM3, IC_R, TIM_IC_IN_TI3);
+	timer_ic_set_input(TIM3, IC_L, TIM_IC_IN_TI4);
+
+	/* Set input capture filter and sampling frequency. */
+	timer_ic_set_filter(TIM3, IC_R, TIM_IC_OFF);
+	timer_ic_set_filter(TIM3, IC_L, TIM_IC_OFF);
+
+	/* Set input capture polarity to noninverted/both edges. */
+	TIM3_CCER |= (TIM_CCER_CC3NP | TIM_CCER_CC3P);
+	TIM3_CCER |= (TIM_CCER_CC4NP | TIM_CCER_CC4P);
+
+	/* Enable input capture. */
+	timer_ic_enable(TIM3, IC_R);
+	timer_ic_enable(TIM3, IC_L);
+
+	/* Enable input capture interrupts. */
+	timer_enable_irq(TIM3, TIM_DIER_CC3IE);
+	timer_enable_irq(TIM3, TIM_DIER_CC4IE);
+
+	/* Enable update interrupt. */
+	timer_enable_irq(TIM3, TIM_DIER_UIE);
+
+	/* Enable counter. */
+	timer_enable_counter(TIM3);
 }
 
 void beacon_setup(void) {
@@ -133,28 +148,28 @@ void beacon_setup(void) {
 }
 
 bool beacon_available(void) {
-	if ((received == BUFFER_LENGTH) || (total >= ((NUM_BITS + 8) * BIT_PERIOD))) {
-		return true;
-	}
-
-	return false;
+	return !is_parsed;
 }
 
 uint16_t beacon_parse(void) {
+	if (is_parsed) {
+		return beacon_id;
+	}
+
+	uint8_t edges = received;
 	uint8_t longest = 0;
 	uint8_t bits = 0;
-	uint16_t length     = 0;
-	uint16_t beacon_id = 0;
+	uint16_t length = 0;
 
-	for (uint8_t i = 1; i < BUFFER_LENGTH; i++) {
+	for (uint8_t i = 1; i < edges; i++) {
 		if (buffer[i] > buffer[longest]) {
 			longest = i;
 			break;
 		}
 	}
 
-	for (uint8_t i = 0; i < BUFFER_LENGTH; i++) {
-		uint16_t pulse = buffer[(longest + i) % BUFFER_LENGTH];
+	for (uint8_t i = 0; i < edges; i++) {
+		uint16_t pulse = buffer[(longest + i) % edges];
 		uint8_t polarity = pulse >> 15;
 		pulse &= ~(1 << 15);
 
@@ -177,5 +192,6 @@ uint16_t beacon_parse(void) {
 		}
 	}
 
+	is_parsed = true;
 	return beacon_id;
 }
--- firmware/locomotive/beacon/beacon.h
+++ firmware/locomotive/beacon/beacon.h
@@ -7,7 +7,6 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/timer.h>
 
 /* CC4NP: Capture/compare 4 complementary output polarity */
