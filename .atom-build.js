6ddecb5a7ff495c18b55213dc7759bb0b3954ebb
Jordi Pakey-Rodriguez <jordi.orlando@gmail.com>
2017-12-26 12:34:36 -0600

    Edit atom-build config

--- .atom-build.js
+++ .atom-build.js
@@ -5,7 +5,33 @@ module.exports = {
     'all'
   ],
   cwd: '{FILE_ACTIVE_PATH}',
-  preBuild: function(replacements) {
+  preBuild: function() {
+    const path = require('path');
+    const editor = atom.workspace.getActiveTextEditor();
+
+    const projectPaths = atom.project.getPaths().map(projectPath => {
+      try {
+        return fs.realpathSync(projectPath);
+      } catch (e) { /* Do nothing. */ }
+      return null;
+    });
+
+    const replacements = {
+      PROJECT_PATH: projectPaths[0]
+    };
+
+    if (editor && (undefined !== editor.getPath())) {
+      replacements.FILE_ACTIVE = fs.realpathSync(editor.getPath());
+      replacements.FILE_ACTIVE_PATH = path.dirname(replacements.FILE_ACTIVE);
+      replacements.PROJECT_PATH = projectPaths.find(p => replacements.FILE_ACTIVE_PATH && replacements.FILE_ACTIVE_PATH.startsWith(p));
+      replacements.FILE_ACTIVE_NAME = path.basename(replacements.FILE_ACTIVE);
+      replacements.FILE_ACTIVE_NAME_BASE = path.basename(replacements.FILE_ACTIVE, path.extname(replacements.FILE_ACTIVE));
+      replacements.SELECTION = editor.getSelectedText();
+    }
+    if (atom.project.getRepositories[0]) {
+      replacements.REPO_BRANCH_SHORT = atom.project.getRepositories()[0].getShortHead();
+    }
+
     let subPath = replacements.FILE_ACTIVE_PATH.replace(replacements.PROJECT_PATH, '');
     for (let dir of ['beacon', 'locomotive']) {
       if (subPath.startsWith('/firmware/' + dir)) {
@@ -21,8 +47,8 @@ module.exports = {
         'ID=0xbcaf'
       ],
       cwd: '{PROJECT_PATH}/firmware/beacon',
-      preBuild: function(replacements) {
-        let id = parseInt(replacements.SELECTION, 16);
+      preBuild: function() {
+        let id = parseInt(editor.getSelectedText(), 16);
         if (!isNaN(id)) {
           this.args[1] = this.args[1].replace('bcaf', id.toString(16).substr(0, 4));
         }
@@ -36,8 +62,8 @@ module.exports = {
         'ID=0xbcaf'
       ],
       cwd: '{PROJECT_PATH}/firmware/beacon',
-      preBuild: function(replacements) {
-        let id = parseInt(replacements.SELECTION, 16);
+      preBuild: function() {
+        let id = parseInt(editor.getSelectedText(), 16);
         if (!isNaN(id)) {
           this.args[2] = this.args[2].replace('bcaf', id.toString(16).substr(0, 4));
         }
